4873e0f3b11fc7a5be9f08f7c4a8bebc61898ced
anamudura <anaandreea1228@yahoo.com>
2023-03-23 08:27:17 +0200

    "second commit"

---
+++ src/main/java/com/example/ps1/registration/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.ps1.registration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/donator").setViewName("donator");
+        registry.addViewController("/donator").setViewName("doctor");
+    }
+}
